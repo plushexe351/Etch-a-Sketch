0477a99ef67975edf922eb6c0572c364a063daa2
Ushnish Tapaswi <126304736+plushexe351@users.noreply.github.com>
2023-05-24 06:37:23 +0530

    Added color modes  and  drag to draw instead of hover

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
-const container = document.querySelector('.container');
-const grid = 16;
-for (let i = 0; i < (grid * grid); i++) {
-    container.innerHTML += `<div class="cell"></div>`;
+const grid = document.querySelector('.grid');
+const jazzy = document.querySelector('.jazzy');
+const grayscale = document.querySelector('.grayscale');
+const def = document.querySelector('.default');
+const eraser = document.querySelector('.eraser');
+const clear = document.querySelector('.clear');
+
+const cells = 40;
+let cellcolor = "#9754cb";
+
+
+for (let i = 0; i < (cells * cells); i++) {
+    grid.innerHTML += `<div class="cell"></div>`;
 }
-document.querySelectorAll('.cell').forEach(cell => {
-    cell.style.cssText = `height : calc(500px / ${grid})`;
-    cell.style.cssText = `width : calc(500px / ${grid})`;
-    cell.addEventListener('mouseover', () => {
-        cell.style.background = `pink`;
+
+def.addEventListener('click', () => cellcolor = "#9754cb")
+grayscale.addEventListener('click', () => cellcolor = "rgb(207, 207, 207)")
+jazzy.addEventListener('click', () => cellcolor = "greenyellow");
+eraser.addEventListener('click', () => cellcolor = "white");
+clear.addEventListener('click', () => {
+    document.querySelectorAll('.cell').forEach(cell => {
+        cell.style.background = "white";
     })
 })
 
-document.querySelector('h1').addEventListener('click', () => {
-    document.querySelectorAll('.cell').forEach(cell => {
-        cell.style.background = "purple";
+let mousedown = false;
+document.querySelectorAll('.cell').forEach(cell => {
+    cell.addEventListener('mousedown', () => mousedown = true)
+    cell.addEventListener('mouseup', () => mousedown = false)
+    // cell.addEventListener('click', () => mousedown = false);
+
+})
+document.querySelectorAll('.cell').forEach(cell => {
+    cell.style.cssText = `height : calc((100vw - 40px)/ ${cells})`;
+    cell.style.cssText = `width : calc((100vw - 40px)/ ${cells})`;
+
+    cell.addEventListener('mouseover', () => {
+        if (mousedown)
+            cell.style.background = cellcolor;
     })
 })
